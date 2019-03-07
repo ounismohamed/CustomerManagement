ad8b55afcd9822497874f063d007a99082c86329
alpha <ounismohamed@gmail.com>
2019-03-07 18:03:23 +0100

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { CustomerFormComponent } from './customer-form/customer-form.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+
+
+const appRoutes: Routes = [
+
+  { path: 'customers', component: CustomersComponent },
+
+  { path: '', component: CustomersComponent },
+
+  { path: 'customer', component: CustomerFormComponent }
+
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    CustomerFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
   ],
   providers: [],
